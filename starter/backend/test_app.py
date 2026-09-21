8324f9188fa9ab7d47122d66647cb25276f222b0
vishnu <vishnu.garuda246@gmail.com>
2026-09-22 01:06:17 +0530

    Deliberate test failure to demonstrate pipeline gating

--- starter/backend/test_app.py
+++ starter/backend/test_app.py
@@ -6,7 +6,7 @@ def test_movies_endpoint_returns_200():
     with app.test_client() as client:
         status_code = os.getenv("FAIL_TEST", 200)
         response = client.get("/movies/")
-        assert response.status_code == status_code
+        assert response.status_code == 500
 
 
 def test_movies_endpoint_returns_json():
